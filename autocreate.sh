71efa6607e1c533eb5f1bc8f99b0747e30ad0c65
Filip Troníček <filip.tronicek@seznam.cz>
2021-11-16 21:38:53 +0100

    Add a Docker script

---
+++ autocreate.sh
@@ -0,0 +1,18 @@
+apt update
+apt upgrade
+
+# install docker
+apt install docker.io
+
+# download the Minecraft world with wget, and extract it
+if [ -n "$1" ]; then
+    wget $1
+    tar -xvf *.tar.gz
+    rm *.tar.gz
+fi
+
+# run the server
+docker run -d -v /root:/data \
+    -e TYPE=SPIGOT \
+    -e EULA=TRUE --name mc itzg/minecraft-server
+    -p 25565:25565
